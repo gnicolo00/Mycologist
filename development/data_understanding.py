32f5d647293c08fe9f9924b1f03722ede124999d
gnicolo00 <ngallotta00@gmail.com>
2024-02-03 12:20:21 +0100

    Added dataset visualization

--- development/data_understanding.py
+++ development/data_understanding.py
@@ -1,3 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import os
+
+# Lettura del dataset contenente le informazioni relative ai funghi, utilizzando la libreria 'os' per garantire una
+# corretta funzionalità in ogni sistema operativo
+dataset_path = os.path.join("..", "datasets", "mushrooms.csv")
+dataset = pd.read_csv(dataset_path)
+print(dataset, end="\n\n---------------------------------------------\n\n")
